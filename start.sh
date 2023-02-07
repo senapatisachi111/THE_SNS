581c3c89cf651a4c365da048fbeaa771b76b8e23
senapatisachi111 <116190773+senapatisachi111@users.noreply.github.com>
2023-02-07 13:39:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/senapatisachi111/THE_SNS /THE_SNS
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /THE_SNS
 fi
-cd /EvaMaria
+cd /THE_SNS
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
