cf81573fc5424ce1667f7178e2f0ee7a45049688
senapatisachi111 <116190773+senapatisachi111@users.noreply.github.com>
2023-02-10 00:52:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -67,7 +67,7 @@ async def start(client, message):
         btn = [
             [
                 InlineKeyboardButton(
-                    "🤖 Join Updates Channel", url=invite_link.invite_link
+                    "🤖 Join Our Main Channel", url="t.me/+lM2prfjyQnRmMDll"
                 )
             ]
         ]
